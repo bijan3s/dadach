f7b2419e0d24ec4316b2a3b903b7924ffe8da7ea
bijan3s <89301589+bijan3s@users.noreply.github.com>
2021-09-17 13:26:39 +0430

    Update urls.py

--- dadach/urls.py
+++ dadach/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('workouts',include('workouts.urls')),
+    path('',include('workouts.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
